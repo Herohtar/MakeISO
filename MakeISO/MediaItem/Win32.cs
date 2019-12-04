005cb50113155618625e19513e9859d2bfaaca82
Herohtar <belac1186@gmail.com>
2019-12-03 22:52:32 -0600

    Remove unused const flags

--- MakeISO/MediaItem/Win32.cs
+++ MakeISO/MediaItem/Win32.cs
@@ -41,9 +41,6 @@ namespace IMAPI2.MediaItem
 
     internal class Win32
     {
-        public const uint SHGFI_ICON = 0x100;
-        public const uint SHGFI_LARGEICON = 0x0;
-        public const uint SHGFI_SMALLICON = 0x1;
 
         [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
         public static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbSizeFileInfo, SHGFI uFlags);
