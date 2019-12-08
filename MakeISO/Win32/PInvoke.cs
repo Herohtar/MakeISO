729172e59925783b8639689456bbbd44d12511bd
Herohtar <belac1186@gmail.com>
2019-12-08 01:42:58 -0600

    Move Native PInvoke into Win32 folder

---
+++ MakeISO/Win32/PInvoke.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Win32
+{
+
+    internal static class PInvoke
+    {
+
+        [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
+        private static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref ShellFileInfo psfi, uint cbSizeFileInfo, GetFileInfoFlags uFlags);
+
+        public static IntPtr GetFileInfo(string filePath, uint fileAttributes, ref ShellFileInfo fileInfo, GetFileInfoFlags flags)
+        {
+            return SHGetFileInfo(filePath, fileAttributes, ref fileInfo, (uint)Marshal.SizeOf(fileInfo), flags);
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool DestroyIcon(IntPtr handle);
+    }
+}
