0308d4342337d89f1d88031ba2720fcfa37250f8
Herohtar <belac1186@gmail.com>
2020-01-04 21:25:53 -0600

    Don't try to cancel a null token source

--- MakeISO/MakeISOTools.cs
+++ MakeISO/MakeISOTools.cs
@@ -470,7 +470,7 @@ namespace MakeISO
         {
             ExecuteDelegate = p =>
             {
-                tokenSource.Cancel();
+                tokenSource?.Cancel();
             },
             CanExecuteDelegate = p => WritingIso
         };
