d47946fa4c41a02011126b5d824d779c2dd99d1d
Herohtar <belac1186@gmail.com>
2020-01-05 23:22:01 -0600

    Add greater than converter

---
+++ MakeISO/Converters/GreaterThanConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MakeISO
+{
+    [ValueConversion(typeof(int), typeof(bool), ParameterType = typeof(int))]
+    public class GreaterThanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var a = System.Convert.ToInt32(value);
+                var b = System.Convert.ToInt32(parameter);
+                return a > b;
+            }
+            catch
+            {
+                return DependencyProperty.UnsetValue;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
